911bdffd4ed1292fb2187c70031aba6175e11ae2
TimFang <mc13735292967@163.com>
2021-05-14 20:12:11 +0800

    merge

---
+++ Dark Aerfaying Remake.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Dark Aerfaying
+// @namespace    https://gitee.com/TimFangDev
+// @version      1.3
+// @description  Dark Aerfaying Theme User JS
+// @author       TimFang4162
+// @match        *://*.aerfaying.com/*
+// @match        *://*.gitblock.cn/*
+// @grant        GM_addStyle
+// @updateURL    https://gitee.com/TimFangDev/DAT/raw/master/Dark%20Aerfaying.user.js
+// ==/UserScript==
+
+(function () {
+  GM_addStyle(`
+  ht
+    `);
+});
