0b378c501bcd16547796b283ddf66201ef75aa26
Silverteal <67404722+Silverteal@users.noreply.github.com>
2021-10-03 18:22:55 +0800

    repair

--- Dark Aerfaying.user.js
+++ Dark Aerfaying.user.js
@@ -2,3 +2,4 @@
 // @name         Aerfaying Evolved
 // @version      Redirect
 // @updateURL    https://raw.fastgit.org/Silverteal/Aerfaying-Evolved/master/Tampermonkey/Aerfaying%20Evolved.user.js
+// ==/UserScript==
